de20a19c97ce62b44f14da69e8b2a0dc2ca4ed54
Tomasz Figa <tomasz.figa@gmail.com>
2014-03-15 00:28:34 +0100

    freedreno: Implement support for OpenFIMG DRM interface
    This patch adds support for OpenFIMG DRM interface to Freedreno libdrm
    code to reduce code duplication between Freedreno and OpenFIMG
    projects, because semantics used by kernel side of both are nearly
    identical.
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>

---
+++ freedreno/of/of_bo.c
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2014 Tomasz Figa <tomasz.figa@gmail.com>
+ *
+ * Parts shamelessly copied from msm backend:
+ *
+ * Copyright (C) 2013 Rob Clark <robclark@freedesktop.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "of_priv.h"
+
+static int of_bo_cpu_prep(struct fd_bo *bo, struct fd_pipe *pipe, uint32_t op)
+{
+	struct fd_device *dev = bo->dev;
+	struct drm_exynos_g3d_cpu_prep req = {
+			.handle = bo->handle,
+			.op = op,
+	};
+
+	get_abs_timeout(&req.timeout, 5000);
+
+	return drmCommandWrite(dev->fd, DRM_EXYNOS_G3D_CPU_PREP,
+				&req, sizeof(req));
+}
+
+static void of_bo_cpu_fini(struct fd_bo *bo)
+{
+	struct fd_device *dev = bo->dev;
+	struct drm_exynos_g3d_cpu_prep req = {
+			.handle = bo->handle,
+	};
+
+	drmCommandWrite(dev->fd, DRM_EXYNOS_G3D_CPU_FINI, &req, sizeof(req));
+}
+
+static void of_bo_destroy(struct fd_bo *bo)
+{
+	struct of_bo *of_bo = to_of_bo(bo);
+	free(of_bo);
+}
+
+static int of_bo_offset(struct fd_bo *bo, uint64_t *offset)
+{
+	struct of_bo *of_bo = to_of_bo(bo);
+
+	if (!of_bo->offset) {
+		struct fd_device *dev = bo->dev;
+		struct drm_mode_map_dumb req = {
+			.handle = bo->handle,
+		};
+		int ret;
+
+		ret = drmIoctl(dev->fd, DRM_IOCTL_MODE_MAP_DUMB, &req);
+		if (ret)
+			return ret;
+
+		of_bo->offset = req.offset;
+	}
+
+	*offset = of_bo->offset;
+
+	return 0;
+}
+
+static struct fd_bo_funcs funcs = {
+		.offset = of_bo_offset,
+		.cpu_prep = of_bo_cpu_prep,
+		.cpu_fini = of_bo_cpu_fini,
+		.destroy = of_bo_destroy,
+};
+
+/* allocate a buffer handle: */
+int of_bo_new_handle(struct fd_device *dev,
+		uint32_t size, uint32_t flags, uint32_t *handle)
+{
+	struct drm_exynos_gem_create req = {
+			.size = size,
+			.flags = EXYNOS_BO_WC,
+	};
+	int ret;
+
+	ret = drmCommandWriteRead(dev->fd, DRM_EXYNOS_GEM_CREATE,
+				  &req, sizeof(req));
+	if (ret)
+		return ret;
+
+	*handle = req.handle;
+
+	return 0;
+}
+
+/* allocate a new buffer object */
+struct fd_bo * of_bo_from_handle(struct fd_device *dev,
+		uint32_t size, uint32_t handle)
+{
+	struct of_bo *of_bo;
+	struct fd_bo *bo;
+	unsigned i;
+
+	of_bo = calloc(1, sizeof(*of_bo));
+	if (!of_bo)
+		return NULL;
+
+	bo = &of_bo->base;
+	bo->funcs = &funcs;
+
+	for (i = 0; i < ARRAY_SIZE(of_bo->list); i++)
+		list_inithead(&of_bo->list[i]);
+
+	return bo;
+}
---
+++ freedreno/of/of_pipe.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2014 Tomasz Figa <tomasz.figa@gmail.com>
+ *
+ * Parts shamelessly copied from msm backend:
+ *
+ * Copyright (C) 2013 Rob Clark <robclark@freedesktop.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "of_priv.h"
+
+static int of_pipe_get_param(struct fd_pipe *pipe,
+		enum fd_param_id param, uint64_t *value)
+{
+	ERROR_MSG("invalid param id: %d", param);
+	return -1;
+}
+
+static int of_pipe_wait(struct fd_pipe *pipe, uint32_t timestamp)
+{
+	struct of_pipe *of_pipe = to_of_pipe(pipe);
+	struct fd_device *dev = pipe->dev;
+	struct drm_exynos_g3d_wait req = {
+			.pipe = of_pipe->pipe,
+			.fence = timestamp,
+	};
+	unsigned long cmd;
+	int ret;
+
+	get_abs_timeout(&req.timeout, 5000);
+
+	if (pipe->id == FD_PIPE_3D)
+		cmd = DRM_EXYNOS_G3D_WAIT;
+	else
+		cmd = DRM_EXYNOS_G2D_WAIT;
+
+	ret = drmCommandWrite(dev->fd, cmd, &req, sizeof(req));
+	if (ret) {
+		ERROR_MSG("wait-fence failed! %d (%s)", ret, strerror(errno));
+		return ret;
+	}
+
+	return 0;
+}
+
+static void of_pipe_destroy(struct fd_pipe *pipe)
+{
+	struct of_pipe *of_pipe = to_of_pipe(pipe);
+	int ret;
+
+	if (of_pipe->pipe) {
+		struct drm_exynos_g3d_pipe req = {
+				.pipe = of_pipe->pipe,
+		};
+		unsigned long cmd;
+
+		if (pipe->id == FD_PIPE_3D)
+			cmd = DRM_EXYNOS_G3D_DESTROY_PIPE;
+		else
+			cmd = DRM_EXYNOS_G2D_DESTROY_PIPE;
+
+		ret = drmCommandWriteRead(pipe->dev->fd, cmd,
+						&req, sizeof(req));
+		if (ret)
+			ERROR_MSG("G3D_DESTROY_PIPE failed! %d (%s)",
+					ret, strerror(errno));
+	}
+
+	free(of_pipe);
+}
+
+static struct fd_pipe_funcs funcs = {
+		.ringbuffer_new = of_ringbuffer_new,
+		.get_param = of_pipe_get_param,
+		.wait = of_pipe_wait,
+		.destroy = of_pipe_destroy,
+};
+
+struct fd_pipe * of_pipe_new(struct fd_device *dev, enum fd_pipe_id id)
+{
+	struct drm_exynos_g3d_pipe req;
+	struct of_pipe *of_pipe = NULL;
+	struct fd_pipe *pipe = NULL;
+	unsigned long cmd;
+	int ret;
+
+	of_pipe = calloc(1, sizeof(*of_pipe));
+	if (!of_pipe) {
+		ERROR_MSG("allocation failed");
+		goto fail;
+	}
+
+	pipe = &of_pipe->base;
+	pipe->funcs = &funcs;
+
+	memset(&req, 0, sizeof(req));
+
+	if (id == FD_PIPE_3D)
+		cmd = DRM_EXYNOS_G3D_CREATE_PIPE;
+	else
+		cmd = DRM_EXYNOS_G2D_CREATE_PIPE;
+
+	ret = drmCommandWriteRead(dev->fd, cmd, &req, sizeof(req));
+	if (ret) {
+		ERROR_MSG("G3D_CREATE_PIPE failed! %d (%s)",
+				ret, strerror(errno));
+		goto fail;
+	}
+
+	of_pipe->pipe = req.pipe;
+
+	return pipe;
+
+fail:
+	if (pipe)
+		fd_pipe_del(pipe);
+	return NULL;
+}
